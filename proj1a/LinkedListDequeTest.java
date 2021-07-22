8e49652c9f3c9d446a07f7fe67784d214f6a397c
劉文儀 <liuwenyi@Cravenbrave-2.local>
2021-07-22 01:11:01 -0400

    proj1A-3

--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -69,7 +69,8 @@ public class LinkedListDequeTest {
 
 //		System.out.println("Running add/remove test.");
 
-//		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+//		System.out.print("Make sure to uncomment the lines ")
+//		System.out.println(	"below (and delete this print statement).");
 
 		LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
 		// should be empty 
